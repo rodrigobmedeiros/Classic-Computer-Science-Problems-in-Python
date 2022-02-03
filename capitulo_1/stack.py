e07c0e8d9c613f0fd4e13e88ac2e556184a1c155
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-02-03 13:57:45 -0300

    Change stack development based on deque to based on list

--- capitulo_1/stack.py
+++ capitulo_1/stack.py
@@ -1,32 +1,30 @@
-from typing import TypeVar, Generic 
+from typing import TypeVar, Generic, List
 
+
+# create a generic type to be used into class construction
 T = TypeVar('T')
 
 class Stack(Generic[T]):
 
     def __init__(self) -> None:
 
-        self.items: list[T] = []
+        self._container: List[T] = []
 
     def push(self, item: T) -> None:
 
-        self.items.append(item)
+        self._container.append(item)
 
-    def pop(self) -> None:
+    def pop(self) -> T:
 
-        return self.items.pop()
+        return self._container.pop()
 
-    def empty(self) -> bool:
+    def __repr__(self) -> None:
 
-        return not self.items
-
-    def __str__(self):
-
-        return f'Stack({str(self.items)})'
+        return repr(self._container)
 
 if __name__ == '__main__':
 
-    stack = Stack[int]()
-
+    stack: Stack[int] = Stack()
     stack.push(1)
-    stack.pop()
+    stack.push(2)
+    stack.push(3)
