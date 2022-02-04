76f9d751f343f7b797afa35ad323740b8daba739
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-02-04 00:31:13 -0300

    hanoi tower implementation

--- capitulo_1/hanoi_tower.py
+++ capitulo_1/hanoi_tower.py
@@ -1,14 +1,43 @@
 from stack import Stack
 
+# init tower_a
+
 num_discs: int = 3
 tower_a: Stack[int] = Stack()
 tower_b: Stack[int] = Stack()
 tower_c: Stack[int] = Stack()
 
-# init tower_a
-
 for disc in range(1, num_discs + 1):
 
     tower_a.push(disc)
 
-print(tower_a)
+def hanoi(begin: Stack[int], end: Stack[int], temp: Stack[int], n: int) -> None:
+
+    if n == 1:
+
+        end.push(begin.pop())
+    
+    else:
+
+        hanoi(begin, temp, end, n - 1)
+        hanoi(begin, end, temp, 1)
+        hanoi(temp, end, begin, n - 1)
+
+if __name__ == '__main__':
+
+    print(
+        ('Inicial condition: \n'
+        f'Tower A: {tower_a} \n'
+        f'Tower B: {tower_b} \n'
+        f'Tower C: {tower_c} \n')
+    )
+
+    hanoi(tower_a, tower_c, tower_b, num_discs)
+    print('\n')    
+    print(
+        ('Final condition: \n'
+        f'Tower A: {tower_a} \n'
+        f'Tower B: {tower_b} \n'
+        f'Tower C: {tower_c} \n')
+    )
+
